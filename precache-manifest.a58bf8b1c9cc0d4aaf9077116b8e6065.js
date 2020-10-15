07ab59fd838811171a30c2e7cfa62ddeeaa5a82e
lavoide <lavoidde@gmail.com>
2020-10-15 19:17:17 +0300

    Updates

---
+++ precache-manifest.a58bf8b1c9cc0d4aaf9077116b8e6065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e645835f7c6268faccab9eb3ad681e",
+    "url": "/react_weather/index.html"
+  },
+  {
+    "revision": "9044731c4415540e30b8",
+    "url": "/react_weather/static/css/main.2f585932.chunk.css"
+  },
+  {
+    "revision": "974f2526a75e2da68801",
+    "url": "/react_weather/static/js/2.a7289688.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/react_weather/static/js/2.a7289688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9044731c4415540e30b8",
+    "url": "/react_weather/static/js/main.6c7a9942.chunk.js"
+  },
+  {
+    "revision": "77839c3496b8d96e3a36",
+    "url": "/react_weather/static/js/runtime-main.5454a14f.js"
+  }
+]);
